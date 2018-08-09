ffc1037010f81156cbafc4805509c6330b0e51da
Maks <anger65@gmail.com>
2018-08-09 13:21:39 +0300

    Второе задание

---
+++ script.js
@@ -0,0 +1,47 @@
+{
+	let yourBudget = prompt("Ваш бюджет на месяц?", "");
+	let shopName = prompt("Название вашего магазина?", "");
+	mainList = {
+		budget: yourBudget,
+		shop: shopName,
+		shopGoods: [],
+		employers: {},
+		open: true
+	}
+// Более правильный способ, т.к. в массиве не будет пустых значений
+
+	for (var i = 0; i < 5; i++) {
+		let a = prompt("Какой тип товаров будем продавать?", "");
+			if ((typeof(a)) === "string" && (a !== null && a != "" && a.length < 50 ) ) {
+				mainList.shopGoods.push(a);
+				console.log("Всё верно");
+		} else {
+				console.log("Не верно!")
+		}
+	}
+
+	// for (let i = 0; i < 5; i++){
+	// 	let a = prompt("Какой тип товаров будем продавать?", "");
+	// 	if ((typeof(a)) === "string" && (a !== null && a != "" && a.length < 50 ) ) {
+	// 		mainList.shopGoods[i] = a;
+	// 		console.log("Всё верно");
+	// 	} else {
+	// 			console.log("Не верно!")
+	// 	}
+	// }
+
+	// while (mainList.shopGoods.length < 5) {
+	// 	let a = prompt("Какой тип товаров будем продавать?", "");
+	// 		if ((typeof(a)) === "string" && (a !== null && a != "" && a.length < 50 ) ) {
+	// 			mainList.shopGoods.push(a);
+	// 			console.log("Всё верно");
+	// 	} else {
+	// 			console.log("Не верно!")
+	// 	}
+	// }
+
+
+
+	alert( "Ваш бюджет на 1 день составляет: " + (mainList.budget/30).toFixed(1) );
+	console.log(mainList);
+}

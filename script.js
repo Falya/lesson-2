67a664ec1ea56e57ace3b13c61b717ff53bb7c04
Maks <anger65@gmail.com>
2018-08-10 00:21:13 +0300

    Второе задание (исправлено)

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-{
 	let yourBudget = prompt("Ваш бюджет на месяц?", "");
 	let shopName = prompt("Название вашего магазина?", "");
-	mainList = {
+	let mainList = {
 		budget: yourBudget,
 		shop: shopName,
 		shopGoods: [],
 		employers: {},
 		open: true
-	}
+	};
 // Более правильный способ, т.к. в массиве не будет пустых значений
 
-	for (var i = 0; i < 5; i++) {
+	for (let i = 0; i < 5; i++) {
 		let a = prompt("Какой тип товаров будем продавать?", "");
 			if ((typeof(a)) === "string" && (a !== null && a != "" && a.length < 50 ) ) {
 				mainList.shopGoods.push(a);
 				console.log("Всё верно");
 		} else {
+			i--
 				console.log("Не верно!")
 		}
 	}
@@ -42,6 +42,5 @@
 
 
 
-	alert( "Ваш бюджет на 1 день составляет: " + (mainList.budget/30).toFixed(1) );
+	alert( "Ваш бюджет на 1 день составляет: " + (mainList.budget / 30).toFixed(1) );
 	console.log(mainList);
-}
